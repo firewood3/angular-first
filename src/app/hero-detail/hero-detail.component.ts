43adbde653819ef276bd3a9b07d52fecec8b8a3c
firewood3 <companyhongsj@gmail.com>
2019-04-08 12:43:14 +0900

    split componet

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from '../hero/hero';
 
 @Component({
-  selector: 'app-hero-detail',
+  selector: 'app-hero-det',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
 
   constructor() { }
 
